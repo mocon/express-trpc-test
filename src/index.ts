56a4aba0e373c57da5deab4c156dd5d5b2a4c2ac
Myles O'Connor <myles@wagerwire.com>
2024-02-20 12:52:42 -0800

    Add tRPC 11

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
 import dotenv from 'dotenv'
+import { appRouter as router, createContext } from './trpc'
 
 dotenv.config()
 
@@ -9,6 +11,14 @@ const port = process.env.PORT || 8080
 app.get('/', (req, res) => {
   res.send({ message: 'Hello, World!' })
 })
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router,
+    createContext,
+  }),
+)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
---
+++ src/trpc.ts
@@ -0,0 +1,26 @@
+import { initTRPC } from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { z } from 'zod'
+
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({})
+
+type Context = Awaited<ReturnType<typeof createContext>>
+
+const t = initTRPC.context<Context>().create()
+
+export const router = t.router
+
+export const publicProcedure = t.procedure
+
+// App router
+export const appRouter = router({
+  hello: publicProcedure.input(z.string()).query((opts) => {
+    opts.input
+    return { message: `Hello, ${opts.input}!` }
+  }),
+})
+
+export type AppRouter = typeof appRouter
