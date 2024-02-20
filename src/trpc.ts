90c7060f04ba02ddcf70330ea6c05a088acd35df
Myles O'Connor <myles@wagerwire.com>
2024-02-20 13:52:17 -0800

    Sentry errors through tRPC

--- src/trpc.ts
+++ src/trpc.ts
@@ -1,4 +1,5 @@
-import { initTRPC } from '@trpc/server'
+import { initTRPC, TRPCError } from '@trpc/server'
+import * as Sentry from '@sentry/node'
 import { CreateExpressContextOptions } from '@trpc/server/adapters/express'
 import { z } from 'zod'
 
@@ -10,13 +11,27 @@ const t = initTRPC.context<Context>().create()
 
 export const router = t.router
 
-export const publicProcedure = t.procedure
+export const middleware = t.middleware
+
+// Sentry
+const sentryMiddleware = middleware(
+  Sentry.Handlers.trpcMiddleware({ attachRpcInput: true }) as any,
+)
+
+export const publicProcedure = t.procedure.use(sentryMiddleware)
 
 // App router
 export const appRouter = router({
   hello: publicProcedure.input(z.string()).query((opts) => {
     opts.input
     return { message: `Hello, ${opts.input}!` }
+  }),
+  debugSentryTrpc: publicProcedure.input(z.string()).query(() => {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An unexpected error occurred, please try again.',
+      // cause: theError,
+    })
   }),
 })
 
